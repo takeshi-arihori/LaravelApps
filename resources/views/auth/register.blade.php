f9960d80a5b5bc284e5aa4e972f04697ac854491
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-10-29 06:41:27 +0900

    feat: プロフィール設定ページの更新 closes #2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,28 +5,32 @@
         <!-- ユーザー名 -->
         <div>
             <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"
+                autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- メールアドレス -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="blo ck mt-1 w-full" type="email" name="email" :value="old('email')"
+                autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- 説明 -->
         <div class="mt-4">
             <x-input-label for="description" :value="__('Description')" />
-            <textarea id="description" class="block mt-1 w-full" name="description" rows="4" required placeholder="{{ __('Describe yourself here.') }}" maxlength="2000">{{ old('description') }}</textarea>
+            <textarea id="description" class="block mt-1 w-full" name="description" rows="4"
+                placeholder="{{ __('Describe yourself here.') }}" maxlength="2000">{{ old('description') }}</textarea>
             <x-input-error :messages="$errors->get('description')" class="mt-2" />
         </div>
 
         <!-- プロフィール画像 -->
         <div class="mt-4">
             <x-input-label for="profile_picture" :value="__('Profile Picture')" />
-            <input id="profile_picture" class="block mt-1 w-full" type="file" name="profile_picture" accept="image/*" required />
+            <input id="profile_picture" class="block mt-1 w-full" type="file" name="profile_picture"
+                accept="image/*" />
             <x-input-error :messages="$errors->get('profile_picture')" class="mt-2" />
         </div>
 
@@ -34,10 +38,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password"
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -46,15 +48,15 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
