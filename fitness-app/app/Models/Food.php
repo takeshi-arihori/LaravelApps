ea5a54f66a8abfb5abba06a3e233449f4e17ab4e
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-11-04 16:22:42 +0900

    feat: データ modelデータの編集・削除

--- fitness-app/app/Models/Food.php
+++ fitness-app/app/Models/Food.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Food extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = []; // 全てのカラムがマスアサインメント可能
+
+    // カロリー計算メソッドの追加
+    public function getTotalCalories(): float
+    {
+        return ($this->protein * 4) + ($this->carbs * 4) + ($this->fat * 9);
+    }
 }
