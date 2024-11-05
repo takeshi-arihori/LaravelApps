180142691780071c294564497dc4260415a3f180
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-11-06 08:43:02 +0900

    feat: hasManyでの関連付けテスト

--- fitness-app/app/Models/FoodType.php
+++ fitness-app/app/Models/FoodType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FoodType extends Model
 {
@@ -14,4 +15,13 @@ class FoodType extends Model
     public $incrementing = false; // 自動インクリメントを無効化
     protected $keyType = 'string'; // キーのデータ型を`string`に設定
     protected $guarded = []; // 全カラムのマスアサインメントを許可
+
+    // Foodとの関係を定義
+    public function food(): HasMany
+    {
+        // このFoodTypeモデルに関連する複数のFoodモデルを取得する関係を定義します。
+        // 'food_type_name'は、Foodモデル側の外部キーです。
+        // 'name'は、FoodTypeモデル側の主キーです。
+        return $this->hasMany(Food::class, 'food_type_name', 'name');
+    }
 }
